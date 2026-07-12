8a6527fc0b24c01af9bfb985ff5a7f3c9e7abd84
bohem dev <microbin98@gmail.com>
2026-07-13 07:05:44 +0900

    chore: initialize package.json for day47 project with Express dependency

--- nodejs-ex/day47/app.js
+++ nodejs-ex/day47/app.js
@@ -1,5 +1,29 @@
-const http = require('http');
+// require('https://expressjs.com/');
+const express = require('express');
 
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/currenttime', function (req, res) {
+  res.send('<h1>' + new Date().toISOString() + '</h1>');
+}); // localhost:3000/currenttime
+
+app.get('/', function (req, res) {
+  res.send(
+    '<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>',
+  );
+}); // localhost:3000/
+
+app.post('/store-user', function (req, res) {
+  const userName = req.body.username;
+  console.log(userName);
+  res.send('<h1>Username stored!</h1>');
+});
+
+app.listen(3000);
+
+/**
 function handleRequest(request, response) {
   if (request.url === '/currenttime') {
     response.statusCode = 200;
@@ -18,3 +42,4 @@ server.listen(3000);
 
 // amazon.com => Send a request to Amazon's server
 // amazon.com:80
+ */
